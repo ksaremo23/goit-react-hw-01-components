0fc55a0b830a77ec45d2ddaeaaa41ae9063c6fec
Leicsha Karen Saremo <120180371+ksaremo23@users.noreply.github.com>
2024-03-10 05:23:15 +0800

    Final Commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,36 @@
-import Profile from "./Profile"
+//PROFILE
+import { Profile } from './Profile/Profile'
+import user from '../data/user.json'
+
+
+//STATISTICS
+import { Statistics } from './Statistics/Statistics'
+import data from '../data/data.json'
+
+//FRIENDLIST
+import { FriendList } from './FriendList/FriendList'
+import friends from '../data/friends.json'
+
+//TRANSACTIONHISTORY
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import transactions from '../data/transaction.json';
 
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Profile />
-    </div>
-  );
-};
+    <>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload Stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
+  )
+}
 
 
